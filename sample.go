7994d184d80b70f5cf3bd454c38015bd81078165
Black-Hole <158blackhole@gmail.com>
2019-05-17 11:14:23 +0800

    Update sample.go

--- sample.go
+++ sample.go
@@ -8,12 +8,12 @@ import(
 
 type MsgContent struct {
     ToUsername string    `xml:"ToUserName"`
-    FromUsername string       `xml:"FromUserName"`
-    CreateTime uint32        `xml:"CreateTime"`
-    MsgType string     `xml:"MsgType"`
-    Content string     `xml:"Content"`
-    Msgid string      `xml:"MsgId"`
-    Agentid uint32     `xml:"AgentId"`
+    FromUsername string    `xml:"FromUserName"`
+    CreateTime uint32    `xml:"CreateTime"`
+    MsgType string    `xml:"MsgType"`
+    Content string    `xml:"Content"`
+    Msgid string    `xml:"MsgId"`
+    Agentid uint32    `xml:"AgentId"`
 }
 
 func main() {
@@ -21,6 +21,7 @@ func main() {
 	receiver_id := "wx5823bf96d3bd56c7"
 	encoding_aeskey := "jWmYm7qr5nMoAUwZRjGtBxmz3KA1tkAj3ykkR6q2B2C"
 	wxcpt := wxbizmsgcrypt.NewWXBizMsgCrypt(token, encoding_aeskey, receiver_id, wxbizmsgcrypt.XmlType)
+
 	/*
 	------------使用示例一：验证回调URL---------------
 	*企业开启回调模式时，企业微信会向验证url发送一个get请求 
@@ -28,7 +29,8 @@ func main() {
 	* GET /cgi-bin/wxpush?msg_signature=5c45ff5e21c57e6ad56bac8758b79b1d9ac89fd3&timestamp=1409659589&nonce=263014780&echostr=P9nAzCzyDtyTWESHep1vC5X9xho%2FqYX3Zpb4yKa9SKld1DsH3Iyt3tP3zNdtp%2B4RPcs8TgAE7OaBO%2BFZXvnaqQ%3D%3D 
 	* HTTP/1.1 Host: qy.weixin.qq.com
 
-	接收到该请求时，企业应		1.解析出Get请求的参数，包括消息体签名(msg_signature)，时间戳(timestamp)，随机数字串(nonce)以及企业微信推送过来的随机加密字符串(echostr),
+	接收到该请求时，企业应
+	1.解析出Get请求的参数，包括消息体签名(msg_signature)，时间戳(timestamp)，随机数字串(nonce)以及企业微信推送过来的随机加密字符串(echostr),
 	这一步注意作URL解码。
 	2.验证消息体签名的正确性 
 	3. 解密出echostr原文，将原文当作Get请求的response，返回给企业微信
@@ -63,7 +65,8 @@ func main() {
 	<AgentID><![CDATA[218]]></AgentID>
 	</xml>
 
-	企业收到post请求之后应该		1.解析出url上的参数，包括消息体签名(msg_signature)，时间戳(timestamp)以及随机数字串(nonce)
+	企业收到post请求之后应该
+	1.解析出url上的参数，包括消息体签名(msg_signature)，时间戳(timestamp)以及随机数字串(nonce)
 	2.验证消息体签名的正确性。
 	3.将post请求的数据进行xml解析，并将<Encrypt>标签的内容进行解密，解密出来的明文即是用户回复消息的明文，明文格式请参考官方文档
 	第2，3步可以用企业微信提供的库函数DecryptMsg来实现。
@@ -108,8 +111,11 @@ func main() {
 	<AgentID>128</AgentID>
 	</xml>
 
-	为了将此段明文回复给用户，企业应：			1.自己生成时间时间戳(timestamp),随机数字串(nonce)以便生成消息体签名，也可以直接用从企业微信的post url上解析出的对应值。
-	2.将明文加密得到密文。	3.用密文，步骤1生成的timestamp,nonce和企业在企业微信设定的token生成消息体签名。			4.将密文，消息体签名，时间戳，随机数字串拼接成xml格式的字符串，发送给企业。
+	为了将此段明文回复给用户，企业应：
+	1.自己生成时间时间戳(timestamp),随机数字串(nonce)以便生成消息体签名，也可以直接用从企业微信的post url上解析出的对应值。
+	2.将明文加密得到密文。
+	3.用密文，步骤1生成的timestamp,nonce和企业在企业微信设定的token生成消息体签名。
+	4.将密文，消息体签名，时间戳，随机数字串拼接成xml格式的字符串，发送给企业。
 	以上2，3，4步可以用企业微信提供的库函数EncryptMsg来实现。
 	*/
 	resp_data := "<xml><ToUserName><![CDATA[mycreate]]></ToUserName><FromUserName><![CDATA[wx5823bf96d3bd56c7]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[this is a test]]></Content><MsgId>1234567890123456</MsgId><AgentID>128</AgentID></xml>"
